526de4feabc9e0007234ab07673e34b50667f176
Leedj <ldj922@naver.com>
2023-09-21 17:53:49 +0900

    Remove momentum folder for developf branch

--- momentum/create_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE raitto_store (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    description TEXT,
-    price DECIMAL(10,2),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
